eeba0e566a180576bb3811316fea1b2898e968bc
Brian Bahk <brian.bahk1334@gmail.com>
2024-05-19 23:15:04 -0500

    styled cards

--- app/(components)/Card.jsx
+++ app/(components)/Card.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const Card = ({ title, content, icon }) => {
   return (
-    <div className="bg-white rounded-lg shadow-lg p-4 flex flex-col items-center justify-center">
+    <div className="bg-black text-white  border-gray-900 border rounded-lg shadow-lg p-4 flex flex-col items-center justify-center">
       <div className="text-2xl mb-2">{icon}</div>
       <h2 className="text-lg font-bold mb-2">{title}</h2>
-      <p className="text-sm">{content}</p>
+      <p className="text-sm text-gray-400">{content}</p>
     </div>
   );
 };
--- app/(components)/InformationPage.jsx
+++ app/(components)/InformationPage.jsx
@@ -11,6 +11,42 @@ const cardsData = [
     content: "This is the content of card 2",
     icon: "🚀",
   },
+  {
+    title: "Card 2",
+    content: "This is the content of card 2",
+    icon: "🚀",
+  },
+  {
+    title: "Card 2",
+    content: "This is the content of card 2",
+    icon: "🚀",
+  },
+  {
+    title: "Card 2",
+    content: "This is the content of card 2",
+    icon: "🚀",
+  },
+  {
+    title: "Card 2",
+    content: "This is the content of card 2",
+    icon: "🚀",
+  },
+  {
+    title: "Card 2",
+    content: "This is the content of card 2",
+    icon: "🚀",
+  },
+  {
+    title: "Card 2",
+    content: "This is the content of card 2",
+    icon: "🚀",
+  },
+  {
+    title: "Card 2",
+    content: "This is the content of card 2",
+    icon: "🚀",
+  },
+
   // Add more card data here
 ];
 
@@ -20,12 +56,14 @@ const InformationPage = () => {
       <div className="text-center text-4xl  text-white font-poppins pt-40 ">
         What we do
       </div>
-    <div>
-        
-    </div>
-      <div className="flex flex-wrap justify-center pt-20">
+      <div className="text-center text-gray-400 font-med px-24 p-4">
+        Fynopsis empowers individual investors worldwide with accessible,
+        data-driven investment solutions utilizing the latest in machine
+        learning and artificial intelligence.
+      </div>
+      <div className="flex flex-wrap justify-center pt-5 px-24">
         {cardsData.map((card, index) => (
-          <div key={index} className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-4 ">
+          <div key={index} className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-3  ">
             <Card title={card.title} content={card.content} icon={card.icon} />
           </div>
         ))}
